0d7a343167a99c4d538b4205f350cc45af08291d
Burima Evgenyi <cuksmail@gmail.com>
2022-09-12 18:42:15 +0500

    ref: changed signature

--- src/core/prompt/prompt.service.ts
+++ src/core/prompt/prompt.service.ts
@@ -2,7 +2,7 @@ import inquirer from 'inquirer';
 import { PromptType } from './prompt.types';
 
 export default class PromptService {
-  public async input<T>(message: string, type: PromptType) {
+  public async input<T>({ message, type }: { message: string, type: PromptType }) {
     const { result } = await inquirer.prompt<{ result: T }>([{
       type,
       name: 'result',
