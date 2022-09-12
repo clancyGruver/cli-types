80a2a41917a3ccb525666e8838853f7396f72a7c
Burima Evgenyi <cuksmail@gmail.com>
2022-09-12 18:42:01 +0500

    ref: service moved to es modules

--- src/core/files/file.servce.ts
+++ src/core/files/file.servce.ts
@@ -1,24 +1,22 @@
-import { promises } from 'fs';
+import { unlink, existsSync } from 'fs';
 import { isAbsolute, dirname, join } from 'path';
 
-export default class FileService {
-  private async isExist(filePath: string) {
-    try {
-      await promises.stat(filePath);
-      return true;
-    } catch {
-      return false;
-    }
+const isExist = (filePath: string) => {
+  try {
+    existsSync(filePath);
+    return true;
+  } catch {
+    return false;
   }
+};
 
-  getFilePath(path: string, name: string, ext: string) {
-    const processedPath = isAbsolute(path) ? path : join(__dirname, '/', path);
-    return join(dirname(processedPath), '/', `${name}.${ext}`);
-  }
+export const getFilePath = (path: string, name: string, ext: string) => {
+  const processedPath = isAbsolute(path) ? path : join(__dirname, '/', path);
+  return join(dirname(processedPath), '/', `${name}.${ext}`);
+};
 
-  async deleteFileIfExist(path: string) {
-    if (await this.isExist(path)) {
-      await promises.unlink(path);
-    }
+export const deleteFileIfExist = (path: string) => {
+  if (isExist(path)) {
+    unlink(path, () => {});
   }
-}
+};
