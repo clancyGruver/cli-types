7801bb1a5958a8c6de6a0bd801a511e2f1df9b2c
Burima Evgenyi <cuksmail@gmail.com>
2022-09-13 16:43:57 +0500

    feat: added ffmpeg converter to app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
+import FfmpegExecutor from './commands/ffmpeg/ffmpeg.executor';
+import { IStreamLogger } from './core/handlers/stream-logger.interface';
+import ConsoleLogger from './out/console/console.logger';
+
 export default class App {
+  constructor(private logger: IStreamLogger) {}
+
   run() {
-    console.log('Done');
+    const exec = new FfmpegExecutor(this.logger);
+    exec.execute();
   }
 }
 
-const app = new App();
+const app = new App(ConsoleLogger.Instanse);
 app.run();
